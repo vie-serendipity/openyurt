24835e657bc882b8d64ded3e26b29cd6bbe25489
vie-serendipity <2733147505@qq.com>
2023-07-10 19:09:05 +0800

    modify

--- pkg/apis/apps/v1alpha1/yurtappconfigurationreplacement_types.go
+++ pkg/apis/apps/v1alpha1/yurtappconfigurationreplacement_types.go
@@ -17,51 +17,52 @@ limitations under the License.
 package v1alpha1
 
 import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 // ImageItem specifies the corresponding container and the claimed image
 type ImageItem struct {
+	// ContainerName represents name of the container 
+	// in which the Image will be replaced
 	ContainerName string `json:"containerName"`
-	// ImageClaim represents the image name which is used by container above.
+	// ImageClaim represents the claimed image name 
+	// which is injected into the container above
 	ImageClaim string `json:"imageClaim"`
 }
 
-// EnvItem specifies the corresponding pool, containerName, and claimed env
+// EnvItem specifies the corresponding container and the claimed env
 type EnvItem struct {
+	// ContainerName represents name of the container 
+	// in which the env will be replaced
 	ContainerName string `json:"containerName"`
-	// EnvClaim represents the detailed enviroment variables that container contains.
+	// EnvClaim represents the detailed enviroment variables container contains
 	EnvClaim map[string]string `json:"envClaim"`
 }
 
-// PersistentVolumeClaimItem
+// PersistentVolumeClaimItem specifies the corresponding container and the claimed pvc
 type PersistentVolumeClaimItem struct {
-	ContainerName string `json:"containerName"`
-	// PVCSource represents volume name.
+	// PVCSource represents pvcClaim name.
 	PVCSource string `json:"pvcSource"`
 	// PVCTarget represents the PVC corresponding to the volume above.
+	// PVCTarget supprot advanced features like wildcard. 
+	// By naming pvc as pvcName-{{nodepool}}, all pvc can be injected at once.
 	PVCTarget string `json:"pvcTarget"`
 }
 
+// ConfigMapItem specifies the corresponding containerName and the claimed configMap
 type ConfigMapItem struct {
-	// ContainerName represents name of the container.
-	ContainerName string `json:"containerName"`
-	// ConfigMapSource represents volume name.
-	ConfigMapSource string `json:"configMapClaim"`
-	// ConfigMapTarget represents the ConfigMap corresponding to the volume above.
+	// ConfigMapSource represents configMap name
+	ConfigMapSource string `json:"configMapSource"`
+	// ConfigMapTarget represents the ConfigMap corresponding to the volume above. 
+	// ConfigMapTarget supprot advanced features like wildcard. 
+	// By naming configMap as configMapName-{{nodepool}}, all configMap can be injected at once.
 	ConfigMapTarget string `json:"configMapTarget"`
 }
 
 type SecretItem struct {
-	// ContainerName represents name of the container.
-	ContainerName string `json:"containerName"`
-	// SecretSource represents volume name.
-	SecretSource string `json:"secretClaim"`
+	// SecretSource represents secret name.
+	SecretSource string `json:"secretSource"`
 	// SecretTarget represents the Secret corresponding to the volume above.
+	// SecretTarget supprot advanced features like wildcard. 
+	// By naming secret as secretName-{{nodepool}}, all secret can be injected at once.
 	SecretTarget string `json:"secretTarget"`
-}
-
-type Replacement struct {
-	Pools []string `json:"pools"`
-	Items []Item   `json:"items"`
 }
 
 // Item represents configuration to be injected.
@@ -76,22 +77,26 @@ type Item struct {
 	UpgradeStrategy       *string                    `json:"upgradeStrategy"`
 }
 
+// Replacement describe a set of nodepools and their shared or identical configurations
+type Replacement struct {
+	Pools []string `json:"pools"`
+	Items []Item   `json:"items"`
+}
+
 type Subject struct {
 	metav1.TypeMeta `json:",inline"`
-	// Name is the name of YurtAppSet or YurtAppDaemon
+	// NameSpace is the namespace of YurtAppSet or YurtAppDaemon
 	NameSpace string `json:"nameSpace"`
+	// Name is the name of YurtAppSet or YurtAppDaemon
 	Name      string `json:"name"`
 }
 
 type YurtAppConfigurationReplacement struct {
 	metav1.TypeMeta `json:",inline"`
-
 	// Standard object's metadata
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	// Describe the object to which this replacement belongs
+	// Describe the object this replacement belongs
 	Subject Subject `json:"subject"`
-
 	// Describe detailed multi-region configuration of the subject above
 	Replacements []Replacement `json:"replacements"`
 }
