f91219c489153ba1e906c1236680962b583cf407
珩轩 <hengxuan.sh@alibaba-inc.com>
2024-01-09 14:05:16 +0800

    fix validator

--- pkg/yurtmanager/controller/loadbalancer/elb/event_handler.go
+++ pkg/yurtmanager/controller/loadbalancer/elb/event_handler.go
@@ -456,8 +456,14 @@ func validateAnnotation(svc *v1.Service) error {
 		}
 	}
 
-	if svc.Spec.Type == v1.ServiceTypeLoadBalancer && svc.Spec.ExternalTrafficPolicy == v1.ServiceExternalTrafficPolicyTypeLocal {
-		if svc.Annotations[Annotation(LoadBalancerId)] != "" {
+	if svc.Annotations[Annotation(LoadBalancerId)] != "" {
+		loadBalancerKey := Annotation(LoadBalancerId)
+		loadBalancerVal := svc.Annotations[loadBalancerKey]
+		if !validateLabels(svc.Annotations[Annotation(LoadBalancerId)]) {
+			fldPath := field.NewPath("meta").Child("annotations").Key(loadBalancerKey)
+			return field.Invalid(fldPath, loadBalancerVal, fmt.Sprintf("correct format, e.g. key1=val1,key2=val2,..."))
+		}
+		if svc.Spec.ExternalTrafficPolicy != v1.ServiceExternalTrafficPolicyTypeLocal {
 			if b, err := strconv.ParseBool(svc.Annotations[Annotation(ListenerOverride)]); err == nil && b {
 				fldPath := field.NewPath("meta").Child("annotations").Key(Annotation(ListenerOverride))
 				return field.Invalid(fldPath, b, fmt.Sprintf("the spec.externalTrafficPolicy must be specified %s if elb is reused", v1.ServiceExternalTrafficPolicyTypeLocal))
@@ -465,12 +471,6 @@ func validateAnnotation(svc *v1.Service) error {
 			if b, err := strconv.ParseBool(svc.Annotations[Annotation(BackendOverride)]); err == nil && b {
 				fldPath := field.NewPath("meta").Child("annotations").Key(Annotation(BackendOverride))
 				return field.Invalid(fldPath, b, fmt.Sprintf("the spec.externalTrafficPolicy must be specified %s if elb is reused", v1.ServiceExternalTrafficPolicyTypeLocal))
-			}
-			loadBalancerKey := Annotation(LoadBalancerId)
-			loadBalancerVal := svc.Annotations[loadBalancerKey]
-			if !validateLabels(svc.Annotations[Annotation(LoadBalancerId)]) {
-				fldPath := field.NewPath("meta").Child("annotations").Key(loadBalancerKey)
-				return field.Invalid(fldPath, loadBalancerVal, fmt.Sprintf("correct format, e.g. key1=val1,key2=val2,..."))
 			}
 		}
 	}
