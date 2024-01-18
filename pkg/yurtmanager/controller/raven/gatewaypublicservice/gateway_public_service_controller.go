78d085f71fbd066c50f5c1be6e99d54eceade0f9
珩轩 <hengxuan.sh@alibaba-inc.com>
2024-01-18 16:45:09 +0800

    release cloud resource should be performed after raven is closed by troopers

--- pkg/yurtmanager/controller/raven/gatewaypublicservice/gateway_public_service_controller.go
+++ pkg/yurtmanager/controller/raven/gatewaypublicservice/gateway_public_service_controller.go
@@ -294,13 +294,9 @@ func (r *ReconcileService) manageService(ctx context.Context, gateway *ravenv1be
 	addSvc = r.bindCloudProduct(addSvc)
 	updateSvc = r.bindCloudProduct(updateSvc)
 	recordServiceNames(specSvcList.Items, record)
-	for i := 0; i < len(addSvc); i++ {
-		if err := r.Create(ctx, addSvc[i]); err != nil {
-			if apierrs.IsAlreadyExists(err) {
-				klog.V(2).Info(Format("service %s/%s has already exist, ignore creating it", addSvc[i].GetNamespace(), addSvc[i].GetName()))
-				return nil
-			}
-			return fmt.Errorf("failed create service for gateway %s type %s , error %s", gateway.GetName(), gatewayType, err.Error())
+	for i := 0; i < len(deleteSvc); i++ {
+		if err := r.Delete(ctx, deleteSvc[i]); err != nil {
+			return fmt.Errorf("failed delete service for gateway %s type %s , error %s", gateway.GetName(), gatewayType, err.Error())
 		}
 	}
 	for i := 0; i < len(updateSvc); i++ {
@@ -308,9 +304,13 @@ func (r *ReconcileService) manageService(ctx context.Context, gateway *ravenv1be
 			return fmt.Errorf("failed update service for gateway %s type %s , error %s", gateway.GetName(), gatewayType, err.Error())
 		}
 	}
-	for i := 0; i < len(deleteSvc); i++ {
-		if err := r.Delete(ctx, deleteSvc[i]); err != nil {
-			return fmt.Errorf("failed delete service for gateway %s type %s , error %s", gateway.GetName(), gatewayType, err.Error())
+	for i := 0; i < len(addSvc); i++ {
+		if err := r.Create(ctx, addSvc[i]); err != nil {
+			if apierrs.IsAlreadyExists(err) {
+				klog.V(2).Info(Format("service %s/%s has already exist, ignore creating it", addSvc[i].GetNamespace(), addSvc[i].GetName()))
+				return nil
+			}
+			return fmt.Errorf("failed create service for gateway %s type %s , error %s", gateway.GetName(), gatewayType, err.Error())
 		}
 	}
 	return nil
