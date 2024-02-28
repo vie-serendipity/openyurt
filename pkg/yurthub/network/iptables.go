38a4aef942442c78b596f8b2b4e4e14850246e9e
rambohe-ch <linbo.hlb@alibaba-inc.com>
2024-02-28 20:30:45 +0800

    enable iptables for dnat traffic to hub agent

--- pkg/yurthub/network/iptables.go
+++ pkg/yurthub/network/iptables.go
@@ -17,14 +17,24 @@ limitations under the License.
 package network
 
 import (
+	"fmt"
+	"net"
+	"strconv"
 	"strings"
 
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
+	listers "k8s.io/client-go/listers/core/v1"
 	"k8s.io/klog/v2"
 	"k8s.io/utils/exec"
 	utilnet "k8s.io/utils/net"
 
 	"github.com/openyurtio/openyurt/pkg/util/iptables"
+)
+
+const (
+	KubeSvcNamespace = "default"
+	KubeSvcName      = "kubernetes"
+	KubeSvcPortName  = "https"
 )
 
 type iptablesRule struct {
@@ -35,36 +45,54 @@ type iptablesRule struct {
 }
 
 type IptablesManager struct {
-	iptables iptables.Interface
-	rules    []iptablesRule
+	kubeSvcClusterIP    string
+	kubeSvcPort         string
+	hubSecureServerAddr string
+	serviceLister       listers.ServiceLister
+	iptables            iptables.Interface
+	rules               []iptablesRule
 }
 
-func NewIptablesManager(dummyIfIP, dummyIfPort string) *IptablesManager {
+func NewIptablesManager(secureServerIP, secureServerPort string, serviceLister listers.ServiceLister) *IptablesManager {
 	protocol := iptables.ProtocolIpv4
-	if utilnet.IsIPv6String(dummyIfIP) {
+	if utilnet.IsIPv6String(secureServerIP) {
 		protocol = iptables.ProtocolIpv6
 	}
 	execer := exec.New()
 	iptInterface := iptables.New(execer, protocol)
 
 	im := &IptablesManager{
-		iptables: iptInterface,
-		rules:    makeupIptablesRules(dummyIfIP, dummyIfPort),
+		iptables:            iptInterface,
+		rules:               []iptablesRule{},
+		serviceLister:       serviceLister,
+		hubSecureServerAddr: net.JoinHostPort(secureServerIP, secureServerPort),
 	}
 
 	return im
 }
 
-func makeupIptablesRules(ifIP, ifPort string) []iptablesRule {
+func makeupIptablesRules(kubeSvcClusterIP, kubeSvcPort, hubSecureServerAddr string) []iptablesRule {
 	return []iptablesRule{
-		// accept traffic to 169.254.2.1:10261/169.254.2.1:10268
-		{iptables.Prepend, iptables.TableFilter, iptables.ChainInput, []string{"-p", "tcp", "-m", "comment", "--comment", "for container access hub agent", "--dport", ifPort, "--destination", ifIP, "-j", "ACCEPT"}},
-		// accept traffic from 169.254.2.1:10261/169.254.2.1:10268
-		{iptables.Prepend, iptables.TableFilter, iptables.ChainOutput, []string{"-p", "tcp", "--sport", ifPort, "-s", ifIP, "-j", "ACCEPT"}},
+		// dnat kubernetes.default svc traffic to 169.254.2.1:10268 in output chain
+		{iptables.Prepend, iptables.TableNAT, iptables.ChainOutput, []string{"-p", "tcp", "-d", kubeSvcClusterIP, "--dport", kubeSvcPort, "-j", "DNAT", "--to-destination", hubSecureServerAddr}},
+		// dnat kubernetes.default svc traffic to 169.254.2.1:10268 in prerouting chain
+		{iptables.Prepend, iptables.TableNAT, iptables.ChainPrerouting, []string{"-p", "tcp", "-d", kubeSvcClusterIP, "--dport", kubeSvcPort, "-j", "DNAT", "--to-destination", hubSecureServerAddr}},
 	}
 }
 
 func (im *IptablesManager) EnsureIptablesRules() error {
+	// at first, prepare kubeSvcClusterIP and kubeSvcPort
+	if len(im.kubeSvcClusterIP) == 0 || len(im.kubeSvcPort) == 0 {
+		if err := im.ResolveKubeSvc(); err != nil {
+			return err
+		}
+	}
+
+	// kubeSvcClusterIP and kubeSvcPort is ready, then prepare iptables rules
+	if len(im.rules) == 0 {
+		im.rules = append(im.rules, makeupIptablesRules(im.kubeSvcClusterIP, im.kubeSvcPort, im.hubSecureServerAddr)...)
+	}
+
 	var errs []error
 	for _, rule := range im.rules {
 		_, err := im.iptables.EnsureRule(rule.pos, rule.table, rule.chain, rule.args...)
@@ -75,6 +103,29 @@ func (im *IptablesManager) EnsureIptablesRules() error {
 		}
 	}
 	return utilerrors.NewAggregate(errs)
+}
+
+func (im *IptablesManager) ResolveKubeSvc() error {
+	svc, err := im.serviceLister.Services(KubeSvcNamespace).Get(KubeSvcName)
+	if err != nil {
+		return err
+	} else if svc == nil {
+		return fmt.Errorf("service %s/%s is not ready, retry later", KubeSvcNamespace, KubeSvcName)
+	} else {
+		im.kubeSvcClusterIP = svc.Spec.ClusterIP
+		for i := range svc.Spec.Ports {
+			if svc.Spec.Ports[i].Name == KubeSvcPortName {
+				im.kubeSvcPort = strconv.FormatInt(int64(svc.Spec.Ports[i].Port), 10)
+				break
+			}
+		}
+		klog.Infof("service %s/%s with clusterIP:Port: %s:%s", KubeSvcNamespace, KubeSvcName, im.kubeSvcClusterIP, im.kubeSvcPort)
+	}
+
+	if len(im.kubeSvcClusterIP) == 0 || len(im.kubeSvcPort) == 0 {
+		return fmt.Errorf("service %s/%s has no clusterIP or Port, Iptables settings will be skipped", KubeSvcNamespace, KubeSvcName)
+	}
+	return nil
 }
 
 func (im *IptablesManager) CleanUpIptablesRules() error {
