b00f6c2a817c1ec49c62d8402086d5fde3108ef3
zyjhtangtang <bingchang.tbc@alibaba-inc.com>
2024-04-15 17:08:38 +0800

    modify status of poolservice (#2019)

--- hack/make-rules/kustomize_to_chart.sh
+++ hack/make-rules/kustomize_to_chart.sh
@@ -196,6 +196,7 @@ EOF
    mv ${crd_dir}/apiextensions.k8s.io_v1_customresourcedefinition_platformadmins.iot.openyurt.io.yaml ${crd_dir}/iot.openyurt.io_platformadmins.yaml
    mv ${crd_dir}/apiextensions.k8s.io_v1_customresourcedefinition_uniteddeployments.apps.openyurt.io.yaml ${crd_dir}/apps.openyurt.io_uniteddeployments.yaml
    mv ${crd_dir}/apiextensions.k8s.io_v1_customresourcedefinition_nodebuckets.apps.openyurt.io.yaml ${crd_dir}/apps.openyurt.io_nodebuckets.yaml
+   mv ${crd_dir}/apiextensions.k8s.io_v1_customresourcedefinition_poolservices.network.openyurt.io.yaml ${crd_dir}/network.openyurt.io_poolservices.yaml
    # TODO: In the future, the crd generation process of yurt-manager and yurt-iot-dock will be split. For now, manually remove it from the yurt-manager script
    # mv ${crd_dir}/apiextensions.k8s.io_v1_customresourcedefinition_devices.iot.openyurt.io.yaml ${crd_dir}/iot.openyurt.io_devices.yaml
    # mv ${crd_dir}/apiextensions.k8s.io_v1_customresourcedefinition_deviceservices.iot.openyurt.io.yaml ${crd_dir}/iot.openyurt.io_deviceservices.yaml
